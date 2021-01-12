0a22974c27aa45e0785b35322ef048f77e2ec11a
Samuel Alves <samuelalves681@gmail.com>
2021-01-12 15:52:52 -0300

    game-java - criado model

---
+++ src/game/model/Calcular.java
@@ -0,0 +1,30 @@
+package game.model;
+
+public class Calcular {
+
+    private int dificulade;
+    private int valor1;
+    private int valor2;
+    private int operacao;
+    private int resultado;
+
+    public int getDificulade() {
+        return dificulade;
+    }
+
+    public int getValor1() {
+        return valor1;
+    }
+
+    public int getValor2() {
+        return valor2;
+    }
+
+    public int getOperacao() {
+        return operacao;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
