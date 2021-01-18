328c99e182211ebb713e52914bf7f05636e99074
Samuel Alves <samuelalves681@gmail.com>
2021-01-18 15:05:57 -0300

    finish text editor

--- src/texteditor/TextEditor.java
+++ src/texteditor/TextEditor.java
@@ -3,15 +3,30 @@ package texteditor;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TextEditor extends JFrame implements ActionListener {
 
     JTextArea textArea;
     JScrollPane scrollPane;
     JSpinner fontSizeSpinner;
+    JLabel fontLabel;
+    JButton fontColorButton;
+    JComboBox fontbox;
+
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
 
     TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,6 +44,8 @@ public class TextEditor extends JFrame implements ActionListener {
         scrollPane.setPreferredSize(new Dimension(450, 450));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
+        fontLabel = new JLabel("Font: ");
+
         fontSizeSpinner = new JSpinner();
         fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
         fontSizeSpinner.setValue(20);
@@ -39,7 +56,39 @@ public class TextEditor extends JFrame implements ActionListener {
             }
         });
 
+        fontColorButton = new JButton("Color");
+        fontColorButton.addActionListener(this);
+
+
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontbox = new JComboBox(fonts);
+        fontbox.addActionListener(this);
+        fontbox.setSelectedItem("Arial");
+
+        /* ---- menuBar ---- */
+
+        menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+        /* ---- /menuBar ---- */
+
+        this.setJMenuBar(menuBar);
+        this.add(fontLabel);
         this.add(fontSizeSpinner);
+        this.add(fontColorButton);
+        this.add(fontbox);
         this.add(scrollPane);
         this.setVisible(true);
     }
@@ -47,5 +96,70 @@ public class TextEditor extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(e.getSource()==fontColorButton){
+            JColorChooser colorChooser = new JColorChooser();
+
+            Color color = colorChooser.showDialog(null, "Choose a color", Color.BLACK);
+
+            textArea.setForeground(color);
+        }
+
+        if(e.getSource()==fontbox){
+            textArea.setFont(new Font((String) fontbox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
+        }
+
+        if(e.getSource()==openItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()){
+                        while(fileIn.hasNextLine()){
+                            String line = fileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                } finally {
+                    fileIn.close();
+                }
+            }
+        }
+
+        if(e.getSource()==saveItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                } finally{
+                    fileOut.close();
+                }
+            }
+        }
+
+        if(e.getSource()==exitItem){
+            System.exit(0);
+        }
     }
 }
